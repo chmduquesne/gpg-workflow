chmduquesne/gpg-workflow

commit a82289baf3677db042532d64e62b977f2ae569e9

Single script for all user messages

Makefile
@@ -27,7 +27,7 @@ KEYID = $(shell gpg -k --with-colons $(UID) | grep "^fpr" | head -n1 | cut -d: -
 KGRIP = $(shell gpg -K --with-colons $(UID) | sed -n 3p | cut -d: -f10)
 
 dumpvars:
-	@./bin/green "Showing the variable values"
+	@./bin/msg green "Showing the variable values"
 	@echo GNUPGHOME = $(GNUPGHOME)
 	@echo UID = $(UID)
 	@echo BACKUPDIR = $(BACKUPDIR)
@@ -39,13 +39,13 @@ dumpvars:
 
 # Check that BACKUPDIR exists and is a git repository
 backupdir:
-	@./bin/green "Making sure the backup path is a git directory"
+	@./bin/msg green "Making sure the backup path is a git directory"
 	test -d $(BACKUPDIR) || mkdir -p $(BACKUPDIR)
 	test -d $(BACKUPDIR)/.git || git init $(BACKUPDIR)
 
 # Save the complete key with its secrets and ownertrust in a new commit
 export: backupdir
-	@./bin/green "Creating a backup of the complete key"
+	@./bin/msg green "Creating a backup of the complete key"
 	gpg --export-secret-keys $(KEYID) > $(BACKUPDIR)/$(KEYID).gpg
 	gpg --export-ownertrust > $(BACKUPDIR)/ownertrust.gpg
 	GIT_DIR=$(BACKUPDIR)/.git GIT_WORK_TREE=$(BACKUPDIR) git add '.'
@@ -53,55 +53,55 @@ export: backupdir
 
 # Restores the complete key and ownertrust
 import: backupdir purge-secrets
-	@./bin/green "Importing a backup of the complete key"
+	@./bin/msg green "Importing a backup of the complete key"
 	gpg --import --import-options restore $(BACKUPDIR)/$(KEYID).gpg
 	gpg --import-ownertrust $(BACKUPDIR)/ownertrust.gpg
 
 # Create a key from scratch
 new-key:
-	@./bin/green "Generating a new key"
+	@./bin/msg green "Generating a new key"
 	gpg --quick-gen-key $(UID) $(ALGORITHM_PRIM) cert never
 
 # Add new encryption/signing/authentication subkeys to the key
 add-subkeys:
-	@./bin/green "Generating subkeys for encr, sign and auth"
+	@./bin/msg green "Generating subkeys for encr, sign and auth"
 	gpg --quick-add-key $(KEYID) $(ALGORITHM_SUB) encr $(EXPIRE)
 	gpg --quick-add-key $(KEYID) $(ALGORITHM_SUB) sign $(EXPIRE)
 	gpg --quick-add-key $(KEYID) $(ALGORITHM_SUB) auth $(EXPIRE)
 
 # Revoke all the subkeys of the key (reason is "key superseded")
 rev-subkeys:
-	@./bin/green "Revoking all subkeys"
+	@./bin/msg green "Revoking all subkeys"
 	./bin/revoke-all-subkeys $(KEYID)
 
 # Transfer all non-expired/non-revoked subkeys to the smartcard
 keystocard:
-	@./bin/green "Transferring all valid subkeys to the smartcard"
+	@./bin/msg green "Transferring all valid subkeys to the smartcard"
 	./bin/transfer-subkeys-to-card $(KEYID)
-	@./bin/blue "You should now run 'make publish'"
+	@./bin/msg blue "You should now run 'make publish'"
 
 # Remove the secret of the master key
 strip-master:
-	@./bin/green "Removing the master secret"
+	@./bin/msg green "Removing the master secret"
 	rm -f ${GNUPGHOME}/private-keys-v1.d/$(KGRIP).key
-	@./bin/blue "You should now run 'make keystocard'"
+	@./bin/msg blue "You should now run 'make keystocard'"
 
 # Remove all secrets from the key
 purge-secrets:
-	@./bin/green "Removing all secrets"
+	@./bin/msg green "Removing all secrets"
 	gpg -K --with-colons $(UID) | grep "^grp" | cut -d: -f10 | \
 		xargs -I% rm -f ${GNUPGHOME}/private-keys-v1.d/%.key
 
 # Revoke the key (requires the secret of the master)
 revoke:
-	@./bin/green "Revoking the master key"
+	@./bin/msg green "Revoking the master key"
 	gpg --gen-revoke $(KEYID) > revocation.txt
 	gpg --import revocation.txt
-	@./bin/red "Key revoked! Use 'make publish' to send it around."
+	@./bin/msg red "Key revoked! Use 'make publish' to send it around."
 
 # Publish the key
 publish:
-	@./bin/green "Publishing the key"
+	@./bin/msg green "Publishing the key"
 	keybase pgp update
 	gpg --keyserver pgp.mit.edu --send-keys $(KEYID)
 	gpg --keyserver hkp://keyring.debian.org --send-key $(KEYID)
@@ -116,14 +116,13 @@ new:
 	$(MAKE) new-key
 	$(MAKE) add-subkeys
 	$(MAKE) export
-	@./bin/blue "To make another backup, run 'BACKUPDIR=/path/to/backup make export'"
-	@./bin/blue "Otherwise run 'make strip-master'"
+	@./bin/msg blue "To make another backup, run 'BACKUPDIR=/path/to/backup make export'"
+	@./bin/msg blue "Otherwise run 'make strip-master'"
 
 # Revoke all subkeys and create new ones
 renew:
-	$(MAKE) import
-	$(MAKE) rev-subkeys
+	$(MAKE) impomsg green(MAKE) rev-subkeys
 	$(MAKE) add-subkeys
 	$(MAKE) export
-	@./bin/blue "To make another backup, run 'BACKUPDIR=/path/to/backup make export'"
-	@./bin/blue "Otherwise run 'make strip-master'"
+	@./bin/msg blue "To make another backup, run 'BACKUPDIR=/path/to/backup make export'"
+	@./bin/msg blue "Otherwise run 'make strip-master'"

bin/blue
@@ -1,3 +1,0 @@
-#!/bin/bash
-
-echo -e "\033[0;34m$@\033[0m"

bin/green
@@ -1,3 +1,0 @@
-#!/bin/bash
-
-echo -e "\033[0;32m$@\033[0m"

bin/msg
@@ -1,0 +1,16 @@
+#!/bin/bash
+
+black=0
+red=1
+green=2
+yellow=3
+blue=4
+magenta=5
+cyan=6
+white=7
+
+color=${!1}
+if [ -n "$color" ]; then
+    shift
+    echo "$(tput bold)$(tput setaf $color)$@$(tput sgr0)"
+fi

bin/red
@@ -1,3 +1,0 @@
-#!/bin/bash
-
-echo -e "\033[0;31m$@\033[0m"

commit 33f3d987589642affc18fc999ee8b4cd4ef08c23

Correcting regression introduced by a82289b

Makefile
@@ -121,7 +121,8 @@ new:
 
 # Revoke all subkeys and create new ones
 renew:
-	$(MAKE) impomsg green(MAKE) rev-subkeys
+	$(MAKE) import
+	$(MAKE) rev-subkeys
 	$(MAKE) add-subkeys
 	$(MAKE) export
 	@./bin/msg blue "To make another backup, run 'BACKUPDIR=/path/to/backup make export'"